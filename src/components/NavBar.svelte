a19374bc866d0d586ef110a2ccdb78341252b0af
Donald Wu <yeukfei02@gmail.com>
2021-11-12 10:56:01 +0800

    fix: 🐛 fix rootUrl var and update readme.md

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,60 +1,85 @@
 <script lang="ts">
-	import { currentPageStore } from '../store';
-	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
-	import IconButton from '@smui/icon-button';
-	import Drawer from './Drawer.svelte';
-	
-	let prominent = false;
-	let dense = false;
-	let secondaryColor = false;
+  import { currentPageStore } from "../store";
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
+  import Drawer from "./Drawer.svelte";
 
-	let drawerOpen = false;
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
 
-	const handleNavBarItemClick = (currentPage: string) => {
-		currentPageStore.set(currentPage);
-	}
+  let drawerOpen = false;
 
-	const handleDrawerOpen = () => {
-		if (!drawerOpen) {
-			drawerOpen = true;
-		} else {
-			drawerOpen = false;
-		}
-	}
+  const handleNavBarItemClick = (currentPage: string) => {
+    currentPageStore.set(currentPage);
+  };
+
+  const handleDrawerOpen = () => {
+    if (!drawerOpen) {
+      drawerOpen = true;
+    } else {
+      drawerOpen = false;
+    }
+  };
 </script>
 
-<style>
-	.flexy {
-		display: flex;
-		flex-wrap: wrap;
-	}
-	.top-app-bar-container {
-		overflow: auto;
-		display: inline-block;
-	}
-	.flexor {
-		display: inline-flex;
-		flex-direction: column;
-	}
-</style>
-
 <div class="flexy">
-	<div class="top-app-bar-container flexor">
-		<TopAppBar variant="static" {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
-			<Row>
-				<Section>
-					<IconButton class="material-icons" on:click={handleDrawerOpen}>menu</IconButton>
-					<Title>Singapore Bus Arrival</Title>
-				</Section>
-				<Section align="end" toolbar>
-					<IconButton class="material-icons" aria-label="Room" on:click={() => handleNavBarItemClick('nearme')}>room</IconButton>
-					<IconButton class="material-icons" aria-label="Search" on:click={() => handleNavBarItemClick('search')}>search</IconButton>
-					<IconButton class="material-icons" aria-label="Favorite" on:click={() => handleNavBarItemClick('favourites')}>favorite</IconButton>
-					<IconButton class="material-icons" aria-label="Settings" on:click={() => handleNavBarItemClick('settings')}>settings</IconButton>
-				</Section>
-			</Row>
-		</TopAppBar>
-	</div>
+  <div class="top-app-bar-container flexor">
+    <TopAppBar
+      variant="static"
+      {prominent}
+      {dense}
+      color={secondaryColor ? "secondary" : "primary"}
+    >
+      <Row>
+        <Section>
+          <IconButton class="material-icons" on:click={handleDrawerOpen}
+            >menu</IconButton
+          >
+          <Title>Singapore Bus Arrival</Title>
+        </Section>
+        <Section align="end" toolbar>
+          <IconButton
+            class="material-icons"
+            aria-label="Room"
+            on:click={() => handleNavBarItemClick("nearme")}>room</IconButton
+          >
+          <IconButton
+            class="material-icons"
+            aria-label="Search"
+            on:click={() => handleNavBarItemClick("search")}>search</IconButton
+          >
+          <IconButton
+            class="material-icons"
+            aria-label="Favorite"
+            on:click={() => handleNavBarItemClick("favourites")}
+            >favorite</IconButton
+          >
+          <IconButton
+            class="material-icons"
+            aria-label="Settings"
+            on:click={() => handleNavBarItemClick("settings")}
+            >settings</IconButton
+          >
+        </Section>
+      </Row>
+    </TopAppBar>
+  </div>
 
-	<Drawer drawerOpen={drawerOpen}/>
+  <Drawer {drawerOpen} />
 </div>
+
+<style>
+  .flexy {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .top-app-bar-container {
+    overflow: auto;
+    display: inline-block;
+  }
+  .flexor {
+    display: inline-flex;
+    flex-direction: column;
+  }
+</style>
--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-    import axios from 'axios';
-    import { getRootUrl, getUniqueId } from '../common/common';
-    import { currentPageStore, busStopCodeStore } from '../store';
+  import axios from "axios";
+  import { getRootUrl, getUniqueId } from "../helpers/helpers";
+  import { currentPageStore, busStopCodeStore } from "../store";
 
-    import Card, {Content, Actions } from '@smui/card';
-    import IconButton, { Icon } from '@smui/icon-button';
-    import Button, { Label } from '@smui/button';
-    import Textfield from '@smui/textfield';
-    import HelperText from '@smui/textfield/helper-text/index';
-    import Snackbar from '@smui/snackbar';
+  import Card, { Content, Actions } from "@smui/card";
+  import IconButton, { Icon } from "@smui/icon-button";
+  import Button, { Label } from "@smui/button";
+  import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text/index";
+  import Snackbar from "@smui/snackbar";
 
-    const ROOT_URL = getRootUrl();
-    const installationId = getUniqueId();
+  const rootUrl = getRootUrl();
+  const installationId = getUniqueId();
 
-    let roadName = '';
-    let place = '';
+  let roadName = "";
+  let place = "";
 
-    let getBusStopByRoadNameResult = null;
-    let getBusStopByDescriptionResult = null;
+  let getBusStopByRoadNameResult = null;
+  let getBusStopByDescriptionResult = null;
 
-    let snackbar: any;
-    let snackbarTitle = '';
+  let snackbar: any;
+  let snackbarTitle = "";
 
-    const getBusStopByRoadName = async (roadName: string) => {
-        let result = null;
+  const getBusStopByRoadName = async (roadName: string) => {
+    let result = null;
 
-        const response = await axios.post(`${ROOT_URL}`, 
-            { 
-                query: `
+    const response = await axios.post(
+      `${rootUrl}`,
+      {
+        query: `
                     query busStopByRoadName ($roadName: String!) {
                         busStopByRoadName (roadName: $roadName) {
                             busStopCode
@@ -38,27 +39,28 @@
                         }
                     }
                 `,
-                variables: { roadName: roadName }
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        if (response) {
-            result = response.data;
-        }
-
-        return result;
+        variables: { roadName: roadName },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response) {
+      result = response.data;
     }
 
-    const getBusStopByDescription = async (place: string) => {
-        let result = null;
+    return result;
+  };
 
-        const response = await axios.post(`${ROOT_URL}`, 
-            { 
-                query: `
+  const getBusStopByDescription = async (place: string) => {
+    let result = null;
+
+    const response = await axios.post(
+      `${rootUrl}`,
+      {
+        query: `
                     query busStopByDescription ($description: String!) {
                         busStopByDescription (description: $description) {
                             busStopCode
@@ -69,197 +71,259 @@
                         }
                     }
                 `,
-                variables: { description: place }
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        if (response) {
-            result = response.data;
-        }
-
-        return result;
+        variables: { description: place },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response) {
+      result = response.data;
     }
 
-    const addFavourites = async (installationId: string, item: any) => {
-        let result = null;
+    return result;
+  };
 
-        const response = await axios.post(`${ROOT_URL}`, 
-            { 
-                query: `
+  const addFavourites = async (installationId: string, item: any) => {
+    let result = null;
+
+    const response = await axios.post(
+      `${rootUrl}`,
+      {
+        query: `
                     mutation addFavourites ($data: AddFavourites!) {
                         addFavourites (data: $data) {
                             status
                         }
                     }
                 `,
-                variables: {
-                    data: {
-                        installationId: installationId,
-                        item: item
-                    }
-                }
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        if (response) {
-            result = response.data;
-        }
-
-        return result;
+        variables: {
+          data: {
+            installationId: installationId,
+            item: item,
+          },
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response) {
+      result = response.data;
     }
 
-    const handleRoadNameInputChange = (e: any) => {
-        if (e.target.value) {
-            roadName = e.target.value;
-            place = '';
-        }
+    return result;
+  };
+
+  const handleRoadNameInputChange = (e: any) => {
+    if (e.target.value) {
+      roadName = e.target.value;
+      place = "";
+    }
+  };
+
+  const handlePlaceInputChange = (e: any) => {
+    if (e.target.value) {
+      place = e.target.value;
+      roadName = "";
+    }
+  };
+
+  const handleSumbitButtonClick = () => {
+    getBusStopByRoadNameResult = null;
+    getBusStopByDescriptionResult = null;
+
+    console.log("roadName = ", roadName);
+    if (roadName && roadName.length > 3) {
+      const result = getBusStopByRoadName(roadName);
+      console.log("result = ", result);
+      getBusStopByRoadNameResult = result;
     }
 
-    const handlePlaceInputChange = (e: any) => {
-        if (e.target.value) {
-            place = e.target.value;
-            roadName = '';
-        }
+    console.log("place = ", place);
+    if (place && place.length > 3) {
+      const result = getBusStopByDescription(place);
+      console.log("result = ", result);
+      getBusStopByDescriptionResult = result;
     }
+  };
 
-    const handleSumbitButtonClick = () => {
-        getBusStopByRoadNameResult = null;
-        getBusStopByDescriptionResult = null;        
+  const handleBusStopCodeClick = (busStopCode: string) => {
+    currentPageStore.set("busArrivalDetails");
+    busStopCodeStore.set(busStopCode);
+  };
 
-        console.log('roadName = ', roadName);
-        if (roadName && roadName.length > 3) {
-            const result = getBusStopByRoadName(roadName);
-            console.log('result = ', result);
-            getBusStopByRoadNameResult = result;
-        }
+  const handleOpenInMapClick = (latitude: number, longitude: number) => {
+    window.open(
+      `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
+    );
+  };
 
-        console.log('place = ', place);
-        if (place && place.length > 3) {
-            const result = getBusStopByDescription(place);
-            console.log('result = ', result);
-            getBusStopByDescriptionResult = result;
-        }
+  const handleAddFavourites = (installationId: string, item: any) => {
+    const result = addFavourites(installationId, item);
+    console.log("result = ", result);
+
+    if (result) {
+      snackbarTitle = "Add favourites success!";
+      snackbar.open();
     }
+  };
 
-    const handleBusStopCodeClick = (busStopCode: string) => {
-        currentPageStore.set('busArrivalDetails');
-        busStopCodeStore.set(busStopCode);
-    }
-
-    const handleOpenInMapClick = (latitude: number, longitude: number) => {
-        window.open(`https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`);
-    }
-
-    const handleAddFavourites = (installationId: string, item: any) => {
-        const result = addFavourites(installationId, item);
-        console.log('result = ', result);
-
-        if (result) {
-            snackbarTitle = 'Add favourites success!';
-            snackbar.open();
-        }
-    }
-    
-    const handleCloseButtonClick = () => {
-        snackbar.close();
-    }
+  const handleCloseButtonClick = () => {
+    snackbar.close();
+  };
 </script>
 
-<style>
-
-</style>
-
 <div class="container">
-    <Card>
-        <Content>
-            <div>
-                <Textfield class="w-100" variant="outlined" bind:value={roadName} on:change={handleRoadNameInputChange} label="Roadname" input$aria-controls="helper-text-outlined-a" input$aria-describedby="helper-text-outlined-a" />
-                <HelperText id="helper-text-outlined-a">Roadname</HelperText>
-            </div>
-            <div>
-                <Textfield class="w-100" variant="outlined" bind:value={place} on:change={handlePlaceInputChange} label="Place" input$aria-controls="helper-text-outlined-a" input$aria-describedby="helper-text-outlined-a" />
-                <HelperText id="helper-text-outlined-a">Place</HelperText>
-            </div>
-            <Actions>
-                <Button class="w-100" on:click={handleSumbitButtonClick} variant="raised"><Label>Submit</Label></Button>
-            </Actions>
-        </Content>
-    </Card>
+  <Card>
+    <Content>
+      <div>
+        <Textfield
+          class="w-100"
+          variant="outlined"
+          bind:value={roadName}
+          on:change={handleRoadNameInputChange}
+          label="Roadname"
+          input$aria-controls="helper-text-outlined-a"
+          input$aria-describedby="helper-text-outlined-a"
+        />
+        <HelperText id="helper-text-outlined-a">Roadname</HelperText>
+      </div>
+      <div>
+        <Textfield
+          class="w-100"
+          variant="outlined"
+          bind:value={place}
+          on:change={handlePlaceInputChange}
+          label="Place"
+          input$aria-controls="helper-text-outlined-a"
+          input$aria-describedby="helper-text-outlined-a"
+        />
+        <HelperText id="helper-text-outlined-a">Place</HelperText>
+      </div>
+      <Actions>
+        <Button
+          class="w-100"
+          on:click={handleSumbitButtonClick}
+          variant="raised"><Label>Submit</Label></Button
+        >
+      </Actions>
+    </Content>
+  </Card>
 
-    {#if getBusStopByRoadNameResult}
-        {#await getBusStopByRoadNameResult}
-            <div class="container my-4">
-                <div class="alert alert-warning" role="alert">
-                    Loading...
+  {#if getBusStopByRoadNameResult}
+    {#await getBusStopByRoadNameResult}
+      <div class="container my-4">
+        <div class="alert alert-warning" role="alert">Loading...</div>
+      </div>
+    {:then data}
+      {#if data.data.busStopByRoadName}
+        {#each data.data.busStopByRoadName as item}
+          <div class="container my-4">
+            <Card>
+              <Content>
+                <div class="my-2" style="font-size: 1.5em; font-weight: bold;">
+                  {item.description}
                 </div>
-            </div>
-        {:then data}
-            {#if data.data.busStopByRoadName}
-                {#each data.data.busStopByRoadName as item }
-                    <div class="container my-4">
-                        <Card>
-                            <Content>
-                                <div class="my-2" style="font-size: 1.5em; font-weight: bold;">{item.description}</div>
-                                <div class="my-2" style="font-size: 1.2em; font-weight: bold;">{item.roadName}</div>
-                                <div class="my-2" style="font-size: 1.2em;">Bus Stop Code: <span class="hoverItem" style="font-size: 1.2em; color: red; text-decoration: underline;" on:click={() => handleBusStopCodeClick(item.busStopCode)}>{item.busStopCode}</span></div>
-                                <span class="hoverItem my-2" style="color: blue; text-decoration: underline;" on:click={() => handleOpenInMapClick(item.latitude, item.longitude)}>Open in map</span>
-                            </Content>
-                            <Actions>
-                                <IconButton on:click={() => handleAddFavourites(installationId, item)} toggle aria-label="Add to favorites" title="Add to favorites">
-                                    <Icon class="material-icons" on>favorite</Icon>
-                                    <Icon class="material-icons">favorite_border</Icon>
-                                </IconButton>
-                            </Actions>
-                        </Card>
-                    </div>
-                {/each}
-            {/if}
-        {/await}
-    {/if}
-
-    {#if getBusStopByDescriptionResult}
-        {#await getBusStopByDescriptionResult}
-            <div class="container my-4">
-                <div class="alert alert-warning" role="alert">
-                    Loading...
+                <div class="my-2" style="font-size: 1.2em; font-weight: bold;">
+                  {item.roadName}
                 </div>
-            </div>
-        {:then data}
-            {#if data.data.busStopByDescription}
-                {#each data.data.busStopByDescription as item }
-                    <div class="container my-4">
-                        <Card>
-                            <Content>
-                                <div class="my-2" style="font-size: 1.5em; font-weight: bold;">{item.description}</div>
-                                <div class="my-2" style="font-size: 1.2em; font-weight: bold;">{item.roadName}</div>
-                                <div class="my-2" style="font-size: 1.2em;">Bus Stop Code: <span class="hoverItem" style="font-size: 1.2em; color: red; text-decoration: underline;" on:click={() => handleBusStopCodeClick(item.busStopCode)}>{item.busStopCode}</span></div>
-                                <span class="hoverItem my-2" style="color: blue; text-decoration: underline;" on:click={() => handleOpenInMapClick(item.latitude, item.longitude)}>Open in map</span>
-                            </Content>
-                            <Actions>
-                                <IconButton on:click={() => handleAddFavourites(installationId, item)} toggle aria-label="Add to favorites" title="Add to favorites">
-                                    <Icon class="material-icons" on>favorite</Icon>
-                                    <Icon class="material-icons">favorite_border</Icon>
-                                </IconButton>
-                            </Actions>
-                        </Card>
-                    </div>
-                {/each}
-            {/if}
-        {/await}
-    {/if}
+                <div class="my-2" style="font-size: 1.2em;">
+                  Bus Stop Code: <span
+                    class="hoverItem"
+                    style="font-size: 1.2em; color: red; text-decoration: underline;"
+                    on:click={() => handleBusStopCodeClick(item.busStopCode)}
+                    >{item.busStopCode}</span
+                  >
+                </div>
+                <span
+                  class="hoverItem my-2"
+                  style="color: blue; text-decoration: underline;"
+                  on:click={() =>
+                    handleOpenInMapClick(item.latitude, item.longitude)}
+                  >Open in map</span
+                >
+              </Content>
+              <Actions>
+                <IconButton
+                  on:click={() => handleAddFavourites(installationId, item)}
+                  toggle
+                  aria-label="Add to favorites"
+                  title="Add to favorites"
+                >
+                  <Icon class="material-icons" on>favorite</Icon>
+                  <Icon class="material-icons">favorite_border</Icon>
+                </IconButton>
+              </Actions>
+            </Card>
+          </div>
+        {/each}
+      {/if}
+    {/await}
+  {/if}
 
-    <Snackbar bind:this={snackbar}>
-        <Label>{snackbarTitle}</Label>
-        <Actions>
-            <Button on:click={handleCloseButtonClick}>Close</Button>
-        </Actions>
-    </Snackbar>
+  {#if getBusStopByDescriptionResult}
+    {#await getBusStopByDescriptionResult}
+      <div class="container my-4">
+        <div class="alert alert-warning" role="alert">Loading...</div>
+      </div>
+    {:then data}
+      {#if data.data.busStopByDescription}
+        {#each data.data.busStopByDescription as item}
+          <div class="container my-4">
+            <Card>
+              <Content>
+                <div class="my-2" style="font-size: 1.5em; font-weight: bold;">
+                  {item.description}
+                </div>
+                <div class="my-2" style="font-size: 1.2em; font-weight: bold;">
+                  {item.roadName}
+                </div>
+                <div class="my-2" style="font-size: 1.2em;">
+                  Bus Stop Code: <span
+                    class="hoverItem"
+                    style="font-size: 1.2em; color: red; text-decoration: underline;"
+                    on:click={() => handleBusStopCodeClick(item.busStopCode)}
+                    >{item.busStopCode}</span
+                  >
+                </div>
+                <span
+                  class="hoverItem my-2"
+                  style="color: blue; text-decoration: underline;"
+                  on:click={() =>
+                    handleOpenInMapClick(item.latitude, item.longitude)}
+                  >Open in map</span
+                >
+              </Content>
+              <Actions>
+                <IconButton
+                  on:click={() => handleAddFavourites(installationId, item)}
+                  toggle
+                  aria-label="Add to favorites"
+                  title="Add to favorites"
+                >
+                  <Icon class="material-icons" on>favorite</Icon>
+                  <Icon class="material-icons">favorite_border</Icon>
+                </IconButton>
+              </Actions>
+            </Card>
+          </div>
+        {/each}
+      {/if}
+    {/await}
+  {/if}
+
+  <Snackbar bind:this={snackbar}>
+    <Label>{snackbarTitle}</Label>
+    <Actions>
+      <Button on:click={handleCloseButtonClick}>Close</Button>
+    </Actions>
+  </Snackbar>
 </div>
+
+<style>
+</style>
